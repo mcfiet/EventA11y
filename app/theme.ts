0f19fd3abc0ef86ae4997a522361dd02910a896e
Fiete Scheel <fietescheel@gmail.com>
2025-07-06 10:31:32 +0200

    feat: Add input styling to theme

--- app/theme.ts
+++ app/theme.ts
@@ -1,5 +1,5 @@
 "use client";
-import { createTheme } from "@mui/material/styles";
+import { alpha, createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   palette: {
@@ -22,6 +22,7 @@ const theme = createTheme({
       black: "#000000",
       black_60: "#666666",
       black_23: "#C4C4C4",
+      black_54: "#757575",
       white: "#FFFFFF",
       white_70: "rgba(255,255,255,0.7)",
       light_blue_1: "#f4f9fe",
@@ -38,6 +39,88 @@ const theme = createTheme({
     fontFamily: ["RedHatDisplay", "sans-serif"].join(","),
   },
   components: {
+    MuiInputLabel: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          fontSize: "16px",
+          fontWeight: 500,
+          color: theme.palette.text.primary,
+          marginBottom: theme.spacing(2),
+          "&.Mui-focused": {
+            color: theme.palette.text.primary,
+          },
+        }),
+      },
+    },
+
+    MuiInputBase: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          border: `2px solid ${theme.palette.custom.black_54}`,
+          borderRadius: 8,
+          fontSize: 16,
+          padding: "12px",
+          fontFamily: theme.typography.fontFamily,
+          color: theme.palette.text.primary,
+          transition: theme.transitions.create(["border-color", "box-shadow"]),
+
+          "&:hover": {
+            borderColor: theme.palette.custom.black_60,
+          },
+          "&.Mui-focused": {
+            borderColor: theme.palette.primary.main,
+            boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
+          },
+          "&.Mui-disabled": {
+            color: theme.palette.text.primary,
+            borderColor: theme.palette.custom.black_23,
+            opacity: 1,
+          },
+        }),
+        input: ({ theme }) => ({
+          padding: 0, // weil Padding schon außen am root
+          fontSize: 16,
+          "&::placeholder": {
+            color: theme.palette.custom.black_54,
+            opacity: 1,
+          },
+        }),
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          borderRadius: 8,
+          "& .MuiOutlinedInput-notchedOutline": {},
+          "&:hover .MuiOutlinedInput-notchedOutline": { border: "none" },
+          "&.Mui-focused .MuiOutlinedInput-notchedOutline": { border: "none" },
+          "&.Mui-disabled": {
+            "& .MuiOutlinedInput-notchedOutline": {},
+            color: theme.palette.text.primary,
+            opacity: 1,
+          },
+        }),
+        input: ({ theme }) => ({
+          padding: "12px",
+          fontSize: 16,
+          color: theme.palette.text.primary,
+          "&::placeholder": {
+            color: theme.palette.custom.black_54,
+            opacity: 1,
+          },
+        }),
+      },
+    },
+    MuiFormHelperText: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          marginLeft: 0,
+          marginTop: "4px",
+          color: theme.palette.custom.black_60,
+          fontSize: "12px",
+        }),
+      },
+    },
     MuiButton: {
       styleOverrides: {
         root: ({ theme }) => ({
@@ -115,6 +198,20 @@ const theme = createTheme({
             src: `url('/fonts/RedHatDisplay/RedHatDisplay-VariableFont_wght.ttf') format('truetype')`,
           },
         ],
+        ".MuiPickersOutlinedInput-root.Mui-focused:not(.Mui-error) .MuiPickersSectionList-root.MuiPickersInputBase-sectionsContainer":
+          {
+            color: theme.palette.custom.black,
+            opacity: 1,
+          },
+        ".MuiPickersSectionList-root.MuiPickersInputBase-sectionsContainer": {
+          color: theme.palette.custom.black_54,
+          opacity: 1,
+        },
+        ".MuiPickersOutlinedInput-root.Mui-focused:not(.Mui-error) .MuiPickersOutlinedInput-notchedOutline":
+          {
+            borderColor: theme.palette.primary.main,
+            boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
+          },
         a: {
           color: theme.palette.secondary.main,
           textDecoration: "none",
