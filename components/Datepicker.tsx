1b5f4e0736e5cf6a771f44fd67615db622d10d58
Fiete Scheel <fietescheel@gmail.com>
2025-07-06 10:32:11 +0200

    feat: add datepicker

---
+++ components/Datepicker.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { Box, Typography } from "@mui/material";
+import { useState } from "react";
+import { Dayjs } from "dayjs";
+import theme from "@/app/theme";
+
+export default function CustomDatePickers() {
+  const [start, setStart] = useState<Dayjs | null>(null);
+  const [end, setEnd] = useState<Dayjs | null>(null);
+
+  return (
+    <Box display="flex" gap={4}>
+      <Box>
+        <Typography variant="body1" fontWeight={500} mb={1}>
+          Veranstaltungsstart
+        </Typography>
+        <DatePicker
+          value={start}
+          onChange={(newValue) => setStart(newValue)}
+          slotProps={{
+            textField: {
+              sx: {
+                ".MuiPickersOutlinedInput-notchedOutline": {
+                  borderRadius: "10px",
+                  border: (theme) =>
+                    `2px solid ${theme.palette.custom.black_54}`,
+                  p: 2,
+                },
+              },
+            },
+            openPickerButton: {
+              sx: {
+                color: theme.palette.primary.main,
+              },
+            },
+          }}
+        />
+      </Box>
+
+      <Box>
+        <Typography variant="body1" fontWeight={500} mb={1}>
+          Veranstaltungsende
+        </Typography>
+        <DatePicker
+          value={end}
+          onChange={(newValue) => setStart(newValue)}
+          slotProps={{
+            textField: {
+              sx: {
+                ".MuiPickersOutlinedInput-notchedOutline": {
+                  borderRadius: "10px",
+                  border: (theme) =>
+                    `2px solid ${theme.palette.custom.black_54}`,
+                  p: 2,
+                },
+              },
+            },
+            openPickerButton: {
+              sx: {
+                color: theme.palette.primary.main,
+              },
+            },
+          }}
+        />
+      </Box>
+    </Box>
+  );
+}
