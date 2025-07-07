1e72427707877b9b417bf6f728f8995da4f83e62
Fiete Scheel <fietescheel@gmail.com>
2025-07-07 15:58:52 +0200

    feat: add location to event interface

--- types/Event.ts
+++ types/Event.ts
@@ -1,14 +1,20 @@
-export interface Event {
+import { Address } from "./Address";
+
+export default interface Event {
   id: string;
   title: string;
   description: string;
   descriptionShort: string;
   startDate: string;
   endDate: string;
-  location: string;
+  location: {
+    name: string;
+    address: Address;
+  };
   image: string;
   imageAlt: string;
   url: string;
   tags: string[];
   ticketNumber: number;
+  accessible: boolean;
 }
