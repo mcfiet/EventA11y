3a500df0adac0a2d8935e41fe2cab2682c866e86
Fiete Scheel <fietescheel@gmail.com>
2025-07-07 14:08:22 +0200

    feat: implement search bar and hero to front page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,33 @@
 import EventCard from "@/components/EventCard";
 import Faqs from "@/components/Faqs";
+import Search from "@/components/Search";
 import { Box, Grid, Typography } from "@mui/material";
 
 export default function Home() {
   return (
     <>
+      <Box
+        component="section"
+        id="hero"
+        sx={{
+          backgroundColor: "transparent",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Box
+          component="img"
+          src="/hero.png"
+          alt="Event das mit einem Smartphone gefilmt wird"
+        />
+        <Search
+          sx={{
+            width: "80%",
+            mt: -2,
+          }}
+        />
+      </Box>
       <Box component="section" id="new-events">
         <Typography component="h2" variant="h2">
           Neue Events
