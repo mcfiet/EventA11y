830110bd4928cc900d9dd7f6e8c78f4ffb2265a9
Fiete Scheel <fietescheel@gmail.com>
2025-07-08 21:54:57 +0200

    fix: change slug to id in event page probs

--- app/event/[id]/page.tsx
+++ app/event/[id]/page.tsx
@@ -1,23 +1,16 @@
-import React from "react";
 import { notFound } from "next/navigation";
 import { events } from "@/data/Events";
 import EventDetailPage from "@/components/EventDetail";
 import Event from "@/types/Event";
 
-interface PageProps {
-  params: {
-    slug: string;
-  };
-}
-
-export default async function Page({ params }: PageProps) {
-  const { id } = params;
-
-  // Finde das Event anhand der ID/Slug
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
   const event = events.find((e: Event) => e.id === id);
-  console.log(id);
   if (!event) {
-    // Wenn nicht gefunden, 404 zurückgeben
     notFound();
   }
 
