cd06877e07db9bb550302b34bde6041ca80875e5
Fiete Scheel <fietescheel@gmail.com>
2025-07-11 07:50:56 +0200

    feat: add heading to event booking

--- app/event/book/[id]/page.tsx
+++ app/event/book/[id]/page.tsx
@@ -2,7 +2,7 @@ import { notFound } from "next/navigation";
 import { events } from "@/data/Events";
 import Event from "@/types/Event";
 import BookingForm from "@/components/BookingForm";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 export default async function Page({
   params,
@@ -16,6 +16,9 @@ export default async function Page({
   }
   return (
     <Box component="section">
+      <Typography component="h2" variant="h2">
+        Event buchen
+      </Typography>
       <BookingForm eventTitle={event.title} maxTickets={event.ticketNumber} />
     </Box>
   );
