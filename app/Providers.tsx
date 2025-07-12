88bc2b74bd4bcaf70be997d2ebdd6401a1673471
Fiete Scheel <fietescheel@gmail.com>
2025-07-12 09:16:41 +0200

    feat: Success Criterion 1.4.3 Contrast (Minimum) snackbar font size

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -2,13 +2,24 @@
 
 import { SnackbarProvider } from "notistack";
 import type { ReactNode } from "react";
+import { MaterialDesignContent } from "notistack";
+import { styled } from "@mui/material";
 
+const StyledMaterialDesignContent = styled(MaterialDesignContent)(() => ({
+  "&.notistack-MuiContent-success": {
+    fontSize: "18px",
+  },
+}));
 export default function Providers({ children }: { children: ReactNode }) {
   return (
     <SnackbarProvider
       maxSnack={3}
       autoHideDuration={1000000}
       anchorOrigin={{ vertical: "top", horizontal: "center" }}
+      style={{
+        fontSize: "20px",
+        fontWeight: "bold",
+      }}
     >
       {children}
     </SnackbarProvider>
