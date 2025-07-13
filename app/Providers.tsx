e4b8a5ad89415202de4f140f69126e9a7a7e0cd1
Fiete Scheel <fietescheel@gmail.com>
2025-07-13 17:09:37 +0200

    feat: snackbar fokus for screenreaders

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -4,32 +4,21 @@ import { closeSnackbar, SnackbarProvider } from "notistack";
 import type { ReactNode } from "react";
 import { MaterialDesignContent } from "notistack";
 import { Button, styled } from "@mui/material";
+import React, { useRef, useEffect } from "react";
 
 const StyledMaterialDesignContent = styled(MaterialDesignContent)(() => ({
   "&.notistack-MuiContent-success": {
     fontSize: "18px",
   },
 }));
+
 export default function Providers({ children }: { children: ReactNode }) {
   return (
     <SnackbarProvider
-      action={(snackbarId) => (
-        <Button
-          sx={{
-            backgroundColor: "background.paper",
-            color: "error.main",
-            py: 1,
-            px: 1,
-            "&:hover": {
-              backgroundColor: "background.default",
-              color: "error.dark",
-            },
-          }}
-          onClick={() => closeSnackbar(snackbarId)}
-        >
-          Schließen
-        </Button>
-      )}
+      Components={{
+        success: StyledMaterialDesignContent,
+      }}
+      action={(snackbarId) => <SnackbarAction snackbarId={snackbarId} />}
       maxSnack={3}
       autoHideDuration={1000000}
       anchorOrigin={{ vertical: "top", horizontal: "center" }}
@@ -40,5 +29,33 @@ export default function Providers({ children }: { children: ReactNode }) {
     >
       {children}
     </SnackbarProvider>
+  );
+}
+
+function SnackbarAction({ snackbarId }: { snackbarId: string | number }) {
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    buttonRef.current?.focus();
+  }, []);
+
+  return (
+    <Button
+      ref={buttonRef}
+      sx={{
+        backgroundColor: "background.paper",
+        color: "error.main",
+        py: 1,
+        px: 1,
+        "&:hover": {
+          backgroundColor: "background.default",
+          color: "error.dark",
+        },
+      }}
+      onClick={() => closeSnackbar(snackbarId)}
+      aria-label="Snackbar schließen"
+    >
+      Schließen
+    </Button>
   );
 }
